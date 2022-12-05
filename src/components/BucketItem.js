11943b8807ad25b48867c5083aa7e32e31a65052
pjarry <pjarry@tbscg.com>
2022-12-05 13:09:54 +0100

    Created variables file, styled item

--- src/components/BucketItem.js
+++ src/components/BucketItem.js
@@ -1,17 +1,25 @@
 
 import { MdClose } from "react-icons/md";
-
+import { MdCheck } from 'react-icons/md';
 
 function BucketItem({item}) {
+  const btnLabel = item.checked ? 'Done' : 'Mark as Done'; 
+  
   return (
-    <li>
+    <li className={item.checked && 'checked'}>
       <div className="info">
         <h3>{item.name}</h3>
         <span className="cat">{item.category}</span>
       </div>
-      <button type="button">
-        <MdClose className="close"/>
-      </button>
+      <div className="action">
+        <button type="button" className={`btn-label ${item.checked ? 'checked' : ''}`}>
+            {btnLabel}
+            <MdCheck/>
+        </button>
+        <button type="button">
+            <MdClose className="close"/>
+        </button>
+      </div>
     </li>
   )
 }
